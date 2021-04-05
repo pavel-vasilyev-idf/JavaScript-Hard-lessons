b954ac011b8b2eff817b222c8bcfa684b56157c1
pavel.vasilyev <pavel.vasilyev@moneyman.ru>
2021-04-05 12:32:24 +0300

    feat: added result task with persons

--- js/script.js
+++ js/script.js
@@ -1,44 +1,55 @@
 'use strict'
 
-let lang = prompt('Введите желаемый язык("ru" либо "en")');
-let ruArr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-let unArr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+// // Task with lang
+
+// let lang = prompt('Введите желаемый язык("ru" либо "en")');
+// let ruArr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+// let unArr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
 
-//with if/else construction
+// //with if/else construction
 
-if (lang == 'ru') {
-    console.log(ruArr);
-    alert(ruArr);
-}
-if (lang == 'en') {
-    console.log(unArr);
-    alert(unArr);
-}
-
-
-// with switch construction
-
-switch (lang) {
-    case 'ru':
-        console.log(ruArr);
-        alert(ruArr);
-        break;
-    case 'en':
-        console.log(unArr);
-        alert(unArr);
-    default:
-        console.log('Вы не выбрали никакой язык');
-        alert('Вы не выбрали никакой язык');
-        break;
-}
+// if (lang == 'ru') {
+//     console.log(ruArr);
+//     alert(ruArr);
+// }
+// if (lang == 'en') {
+//     console.log(unArr);
+//     alert(unArr);
+// }
 
 
-// through a multidimensional array
-let multiArr = {
-    'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
-    'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
-}
+// // with switch construction
 
-alert(multiArr[lang]);
-console.log(multiArr[lang]);
+// switch (lang) {
+//     case 'ru':
+//         console.log(ruArr);
+//         alert(ruArr);
+//         break;
+//     case 'en':
+//         console.log(unArr);
+//         alert(unArr);
+//     default:
+//         console.log('Вы не выбрали никакой язык');
+//         alert('Вы не выбрали никакой язык');
+//         break;
+// }
+
+
+// // through a multidimensional array
+// let multiArr = {
+//     'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+//     'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+// }
+
+// alert(multiArr[lang]);
+// console.log(multiArr[lang]);
+
+
+
+
+// Task with Person
+
+let namePerson = prompt("Введите имя, чтобы узнать должность");
+
+namePerson === "Артем" ? console.log(`${namePerson} - "директор"`) : namePerson === "Максим" ? console.log(`${namePerson} - "преподаватель"`) : console.log(`${namePerson} - "студент"`);
